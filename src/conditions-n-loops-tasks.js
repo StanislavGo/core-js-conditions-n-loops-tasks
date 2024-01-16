adcb4076c9ea31138613f08d9fe528a574a4a0c0
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-16 23:13:54 +0200

    feat: finish one function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -84,8 +84,11 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a <= 0 || b <= 0 || c <= 0) return false;
+  if (a + b <= c || c + b <= a || a + c <= b) return false;
+
+  return true;
 }
 
 /**
