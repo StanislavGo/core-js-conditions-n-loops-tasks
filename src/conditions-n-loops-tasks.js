1f23ce4f1b65d60950e630fdfd028ed94382909a
“Stanislav <gorokht6mail.ru@gmail.com>
2024-01-17 22:19:52 +0200

    feat: create isContainNumber function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -257,8 +257,23 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let number = num;
+  if (digit < 0 || digit > 9) {
+    return false;
+  }
+
+  while (number > 0) {
+    const currentDigit = number % 10;
+
+    if (currentDigit === digit) {
+      return true;
+    }
+
+    number = Math.floor(number / 10);
+  }
+
+  return false;
 }
 
 /**
